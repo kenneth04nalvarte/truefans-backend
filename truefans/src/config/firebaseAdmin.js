b03195a82dfc498d1bca0fb655b060e2a284cc87
kenne <kenneth.nalvarte.1@gmail.com>
2025-05-27 12:13:45 -0600

    Fix: Prevent duplicate Firebase Admin initialization (final, exact)

--- truefans/src/config/firebaseAdmin.js
+++ truefans/src/config/firebaseAdmin.js
@@ -2,9 +2,11 @@ const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
 
 const db = admin.firestore();
 
