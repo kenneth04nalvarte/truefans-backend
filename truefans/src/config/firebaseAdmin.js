3b22475f41b591b0db049419e63f4954644e8137
kenne <kenneth.nalvarte.1@gmail.com>
2025-05-27 11:31:39 -0600

    Use FIREBASE_SERVICE_ACCOUNT env variable for Firebase Admin initialization

--- truefans/src/config/firebaseAdmin.js
+++ truefans/src/config/firebaseAdmin.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -7,4 +8,4 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-module.exports = { admin, db }; 
+module.exports = { admin, db };
